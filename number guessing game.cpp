253da66b1d066e645ac77c169c9f39653b17c609
Tirth_patel <129929113+patel1000@users.noreply.github.com>
2023-08-14 13:00:47 +0530

    Update number guessing game.cpp

--- number guessing game.cpp
+++ number guessing game.cpp
@@ -2,32 +2,30 @@
 #include <cstdlib>
 
 int main() {
-    int number ;
+    int number;
     int min = 1;
     int max = 100;
-    bool success = 0 ;
+    bool success = false;
     int unknown = std::rand() % (max - min + 1) + min;
 
-    std::cout << "number is between 1 to 100" << std::endl;
- do (
-        std::cout << "Guess the number: " << std::ends;
+    std::cout << "Number is between 1 to 100" << std::endl;
+    
+    do {
+        std::cout << "Guess the number: ";
         std::cin >> number;
 
-        if ( unknown == number ){
-            std::cout << "correct" << std::endl; 
-            success = 1;
-            break ;
+        if (unknown == number) {
+            std::cout << "Correct!" << std::endl;
+            success = true;
+            break;
+        } else if (number > unknown) {
+            std::cout << "You guessed higher. The secret number is lower than your guess." << std::endl;
+        } else if (number < unknown) {
+            std::cout << "You guessed lower. The secret number is higher than your guess." << std::endl;
         }
+    } while (!success);
 
-        else if ( number > unknown ){
-            std::cout << "you guessed higher, secret number is lower than you guessed" << std::endl;
-            std::cout << "chances left :" << chance - 1 << std::endl;
-        }
-        else if ( number < unknown ){
-            std::cout << "you guessed lower, secret number is higher than you guessed" << std::endl;
-        }
-       ) 
- While ( success == 0) 
-        std::cout << "congratulations, you are right" << std::endl;  
+    std::cout << "Congratulations, you guessed the right number!" << std::endl;  
+
     return 0;
 }
